5aaa72ed0e667ee88443ca82e58de438d879f35c
Bapi Das <Bapi.Das@softwareag.com>
2019-09-07 12:39:56 +0530

    added interview 2 puzzles

---
+++ src/com/bada/dsa/practice/node/InterviewPuzzleHelperMethods.java
@@ -0,0 +1,119 @@
+package com.bada.dsa.practice.node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InterviewPuzzleHelperMethods {
+
+}
+
+/**
+ * Helper Classes
+ * 
+ * @author bada
+ *
+ */
+
+class BinaryNode {
+	int data;
+	BinaryNode left, right;
+
+	public BinaryNode(int value) {
+		this.data = value;
+		left = right = null;
+	}
+}
+
+/**
+ * The problem can also be solved using simple recursive traversal. We can keep
+ * track of level of a node by passing a parameter to all recursive calls. The
+ * idea is to keep track of maximum level also. And traverse the tree in a
+ * manner that right/left subtree is visited before left/right subtree depending
+ * on what view you want. Whenever we see a node whose level is more than
+ * maximum level so far, we print the node because this is the last node in its
+ * level (Note that we traverse the right subtree before left subtree for right
+ * view and vice versa for left view).
+ * 
+ * @author bada
+ *
+ */
+class BinaryTree {
+
+	BinaryNode root;
+	int maxLevel;
+	static final int left_view = 0;
+	static final int right_view = 1;
+
+	void treeView(BinaryNode node, int level, int view) {
+		if (node == null) {
+			return;
+		}
+
+		if (maxLevel < level) {
+			System.out.print(node.data + " ");
+			maxLevel = level;
+		}
+
+		if (view == left_view) {
+			treeView(node.left, level + 1, view);
+			treeView(node.right, level + 1, view);
+		} else if (view == right_view) {
+			treeView(node.right, level + 1, view);
+			treeView(node.left, level + 1, view);
+		}
+
+	}
+
+	void treeView(int view) {
+		String viewName = "right";
+		if (view == 0) {
+			viewName = "left";
+		}
+
+		System.out.print("Tree " + viewName + " View: ");
+		treeView(root, view);
+		System.out.println();
+	}
+
+	void treeView(BinaryNode node, int view) {
+		treeView(node, 1, view);
+	}
+
+	/**
+	 * If we observe carefully, we will see that our main task is to print the right
+	 * most node of every level. So, we will do a level order traversal on the tree
+	 * and print the rightmost node at every level.
+	 * 
+	 * @param root
+	 */
+	void printRightViewWithQueue() {
+		if (root == null)
+			return;
+
+		Queue<BinaryNode> queue = new LinkedList<>();
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+			// number of nodes at current level
+			int n = queue.size();
+
+			// Traverse all nodes of current level
+			for (int i = 1; i <= n; i++) {
+				BinaryNode temp = queue.poll();
+
+				// Print the right most element at
+				// the level
+				if (i == n)
+					System.out.print(temp.data + " ");
+
+				// Add left node to queue
+				if (temp.left != null)
+					queue.add(temp.left);
+
+				// Add right node to queue
+				if (temp.right != null)
+					queue.add(temp.right);
+			}
+		}
+	}
+}
--- src/com/bada/dsa/practice/node/InterviewPuzzles.java
+++ src/com/bada/dsa/practice/node/InterviewPuzzles.java
@@ -4,7 +4,59 @@ import java.util.Random;
 
 public class InterviewPuzzles {
 	public static void main(String[] args) {
-		SortRandomGenerateNumbers();
+//		SortRandomGenerateNumbers();
+//		printRightViewOfABinaryTree();
+//		printLeftViewOfABinaryTree();
+//		printRightViewOfABinaryTreeUsingQueue();
+	}
+
+	private static void printRightViewOfABinaryTreeUsingQueue() {
+		BinaryTree tree = new BinaryTree();
+		tree.root = new BinaryNode(10);
+		tree.root.left = new BinaryNode(2);
+		tree.root.right = new BinaryNode(3);
+		tree.root.left.left = new BinaryNode(7);
+		tree.root.left.right = new BinaryNode(8);
+		tree.root.right.right = new BinaryNode(15);
+		tree.root.right.left = new BinaryNode(12);
+		tree.root.right.right.left = new BinaryNode(14);
+		tree.printRightViewWithQueue();
+	}
+
+	/**
+	 * Print Left View of a Binary Tree
+	 */
+	private static void printLeftViewOfABinaryTree() {
+		BinaryTree tree = new BinaryTree();
+		tree.root = new BinaryNode(1);
+		tree.root.left = new BinaryNode(2);
+		tree.root.right = new BinaryNode(3);
+		tree.root.left.left = new BinaryNode(4);
+		tree.root.left.right = new BinaryNode(5);
+//		tree.root.left.right.left=new BinaryNode(50);
+		tree.root.right.left = new BinaryNode(6);
+		tree.root.right.right = new BinaryNode(7);
+		tree.root.right.right.right = new BinaryNode(8);
+
+		tree.treeView(0);
+	}
+
+	/**
+	 * Print Right View of a Binary Tree
+	 */
+	private static void printRightViewOfABinaryTree() {
+		BinaryTree tree = new BinaryTree();
+		tree.root = new BinaryNode(1);
+		tree.root.left = new BinaryNode(2);
+		tree.root.right = new BinaryNode(3);
+		tree.root.left.left = new BinaryNode(4);
+		tree.root.left.right = new BinaryNode(5);
+//		tree.root.left.right.left=new BinaryNode(50);
+		tree.root.right.left = new BinaryNode(6);
+		tree.root.right.right = new BinaryNode(7);
+		tree.root.right.right.right = new BinaryNode(8);
+
+		tree.treeView(1);
 	}
 
 	/**
